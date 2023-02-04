bed2a20f32779698c63201c87e4407f6baecfa33
codebender <lionelmbankeu@gmail.com>
2023-02-04 23:21:21 +0200

    authentication added to middleware

--- .frame/index.js
+++ .frame/index.js
@@ -1,4 +1,5 @@
 import {serve} from "https://deno.land/std/http/server.ts";
+import { getCookies } from "https://deno.land/std/http/cookie.ts";
 import "https://deno.land/std/dotenv/load.ts";
 
 let resp;
@@ -18,11 +19,29 @@ const service = async (ext, pathname, req) => {
   
 }
 
+const isAuthenticated = (req) => {
+   const {id} = getCookies(req.headers)
+
+   return id ? true : false
+
+}
+
 const middleware = async (request, info) => {
   
         const { pathname } = new URL(request.url);
         window.extPath = window?._cwd ? window._cwd: Deno.cwd() 
- 
+        
+        if(!isAuthenticated(request)){
+
+            console.log(Deno.env.get('env'))
+            return Response.redirect(`https://${Deno.env.get('env') ? 'localhost:9001/account?domain=app.sauveur&redirect=localhost:9001?domain=dash.sauveur' : 'app.sauveur.xyz/account?redirect=dash.sauveur.xyx'} `)
+            // return new Response(null,{
+            //     status: 401,
+            //     headers: {
+            //         'WWW-Authenticate': 'Basic realm="Access to staging site"'
+            //     }
+            // })
+        }
 
         try{ 
             const extensions = Deno.env.get('env') ? await import(`${window.extPath}/extensions.js`) : await import(`${window.extPath}/ext.js`)
