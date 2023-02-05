a1f004db1cb429c44306fa4505907710d83149f4
codebender <lionelmbankeu@gmail.com>
2023-02-05 19:30:19 +0200

    i think were good

--- .frame/index.js
+++ .frame/index.js
@@ -17,6 +17,7 @@ const service = async (ext, pathname, req) => {
 };
 
 const middleware = async (request, info) => {
+<<<<<<< HEAD
   const { pathname } = new URL(request.url);
   window.extPath = window?._cwd ? window._cwd : Deno.cwd();
 
@@ -33,6 +34,40 @@ const middleware = async (request, info) => {
     return Response.json({ msg: "Error:LEVEL1" }, { status: 500 });
   }
 };
+=======
+  
+        const { pathname } = new URL(request.url);
+        window.extPath = window?._cwd ? window._cwd: Deno.cwd() 
+  
+        if(!isAuthenticated(request)){
+
+            const {pathname} = new URL(request.url)
+            
+     
+            const {default:app} = await import('app.sauveur.dev/index.js')
+            const appReq = new Request(`https://app.sauver.xyz/account?redirect=${request.headers.get('host')}`,{
+                headers:{
+                    host: 'app.sauver.xyz'
+                }
+            })
+ 
+            window._cwd = 'app.sauveur.dev'
+            if(pathname !== '/'){
+             
+                return app(request)
+            }
+
+
+            return app(appReq)
+            // return Response.redirect(`https://${Deno.env.get('env') ? 'app-sauveur.localhost:9001/account?redirect=dash-sauveur.localhost:9001' : 'app.sauveur.xyz/account?redirect=dash.sauveur.xyx'} `)
+            // // return new Response(null,{
+            // //     status: 401,
+            // //     headers: {
+            // //         'WWW-Authenticate': 'Basic realm="Access to staging site"'
+            // //     }
+            // // })
+        }
+>>>>>>> a4cdd34 (they might be changes)
 
 if (import.meta.main) {
   const port = 9090;
